6c4e3f54cca88feb4585387d0aff3d7414767aa0
Muhammad Uzair Danyal <muzaird@gmail.com>
2021-10-18 16:10:57 +0500

    login page

---
+++ www/js/routes.js
@@ -0,0 +1,12 @@
+var routes = [
+
+        {
+            path: '/',
+            url: './index.html',
+            name: 'home',
+          },
+        {
+          path: '/signin/',
+          url: './pages/signin.html',
+        }
+      ]
