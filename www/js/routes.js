f10c400aa610bae9b73954c3cd70cceab3513e56
=Arham Khan <=>
2021-10-19 02:20:06 -0700

    header added

--- www/js/routes.js
+++ www/js/routes.js
@@ -8,5 +8,11 @@ var routes = [
         {
           path: '/signin/',
           url: './pages/signin.html',
+          name:'signin'
+        },
+        {
+          path: '/home/',
+          url: './pages/home.html',
+          name:'home'
         }
       ]
